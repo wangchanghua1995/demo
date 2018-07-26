cb27904748bc2424cd8608d28145cc4a7c93e3ee
wangchanghua1995 <17610788268@163.com>
2018-07-26 11:00:55 +0800

    对齐方向2

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: left;
+    text-align: center;
     margin-top: 60px;
     height: 100%;
 
